bc867388be656201a994b5f5ab5746988c93224f
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-22 17:32:05 +0200

    feat: add hero (with placeholder img)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script>
     import AppHeader from "./components/AppHeader.vue"
+    import AppHero from "./components/AppHero.vue"
 
     export default{
         components: {
-            AppHeader
+            AppHeader,
+            AppHero
         }
     }
 </script>
 
 <template>
     <AppHeader></AppHeader>
+    <main>
+        <AppHero></AppHero>
+    </main>
 </template>
 
 <style lang="scss">
     @use './style/general' as *;
+
+    main{
+        background-image: url(./assets/img/body-bg.jpg);
+        background-size: cover;
+    }
 </style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,66 @@
+<script>
+    export default{
+        name: 'AppHero'
+    }
+</script>
+
+<template>
+    <div class="hero">
+        <div class="container">
+            <div class="hero-content">
+                <div class="badge">
+                    <div class="badge-content">Upcoming Stack</div>
+                </div>
+                <h2>Resident Evil 3 Remake</h2>
+                <button type="button">Shop Now</button>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    @use '../style/partials/variables' as *;
+
+    .container{
+        display: flex;
+        align-items: center;
+    }
+
+    .hero{
+        height: 750px;
+        overflow: hidden;
+        background-image: url(../assets/img/parallax.jpg); // ! PLACEHOLDER IMG - ASK FOR THE CORRECT ONE
+        background-position: center;
+    }
+
+    button{
+        background-color: $main_text;
+        transition: 300ms;
+        padding: 12px 28px;
+        &:hover{
+            background-color: $primary_color;
+            position: relative;
+            bottom: 3px;
+            box-shadow: 0px 2px 2px black;
+        }
+    }
+
+    h2{
+        font-weight: 500;
+        font-size: 4rem;
+        margin-bottom: 100px;
+        margin-top: 30px;
+    }
+
+    .hero-content{
+        width: 50%;
+    }
+
+    .badge{
+        padding: 3px 20px;
+        .badge-content{
+            font-size: 1.7rem;
+        }
+    }
+</style>
